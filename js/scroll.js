458b10c7f15bc2698c58d52b1256876127cf7ceb
chika.katayama <kitamurachikamu@gmail.com>
2023-04-01 18:04:07 +0900

    “sotsugyo_test1”

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+$('#js-nav a').on('click', function () { 
+  let hrefElement = $(this).attr('href');
+  let headerHeight = $('#header').outerHeight(true);
+  let position = $(hrefElement).offset().top - headerHeight;
+  $('body,html').animate({
+    scrollTop: position
+  }, 500);
+  return false;
+});
